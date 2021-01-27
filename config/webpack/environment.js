85171967127dfe0c6648985d2e415da36fb1fe95
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-28 03:09:26 +0900

    floccsのcss設計に変更

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
+// jqueryを使える設定
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery'
   })
+
 )
+
+// sassを複数importする設定
+// $ yarn add import-glob-loaderをしてから
+const globCssImporter = require('node-sass-glob-importer');
+
+environment.loaders.get('sass')
+  .use
+  .find(item => item.loader === 'sass-loader')
+  .options
+  .sassOptions = {importer: globCssImporter()};
+
 module.exports = environment

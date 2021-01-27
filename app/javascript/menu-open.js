512725b77571014a8886dd248f0d06b4c5191f11
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-01-28 07:26:19 +0900

    リファクタリング

--- app/javascript/menu-open.js
+++ app/javascript/menu-open.js
@@ -1,11 +1,11 @@
 $(function () {
   if (location.pathname.match("/")) {
     // メニュー表示のアニメーション
-    $('.js_menu-open_NavBtn_trigger').on('click', function () {
+    $('.JS_menu-open_trigger').on('click', function () {
       const $clickMenu = $(this);
-      const $openMenu = $('.js_menu-open_NavMenu_target');
-      $clickMenu.toggleClass('isActive-button');
-      $openMenu.toggleClass('isOpen-menu');
+      const $openMenu = $('.JS_menu-open_target');
+      $clickMenu.toggleClass('C_button--hamberger--isActive');
+      $openMenu.toggleClass('isOpen');
     });
   };
 });
--- app/javascript/scroll.js
+++ app/javascript/scroll.js
@@ -1,17 +1,18 @@
 $(function () {
-  const top_height = $('.js_scroll_TopImage_height_get').outerHeight(true);
-  const CatchWrapTop_height = $('.js_scroll_CatchCopy__wrapper-top_height_get').
+  const top_height = $('.JS_scroll_TopImage_get').outerHeight(true);
+  const CatchWrapTop_height = $('.JS_scroll_CatchCopy_get').
     outerHeight(true);
-  const $TopImageMessage = $('.js_scroll_message_height_get')
+  const $TopImageMessage = $('.JS_scroll_message_get')
   const TopImageMessage_height = $TopImageMessage.
     outerHeight(true);
   const TopImageMessage_y = $TopImageMessage.offset().top;
+
   const moving_distance = top_height + (CatchWrapTop_height / 2) - TopImageMessage_y - (TopImageMessage_height * 0.5);
 
   // キャッチコピーの上下移動
-  gsap.to(".js_scroll_message_move_target",{
+  gsap.to(".JS_scroll_move_target",{
     scrollTrigger: {
-      trigger: ".js_scroll_TopImage_trigger",
+      trigger: ".JS_scroll_trigger",
       start: 'center+=' + (TopImageMessage_height * 0.5) + ' center',
       end: 'bottom center',
       scrub: true,
@@ -22,19 +23,19 @@ $(function () {
   });
   // ヘッダー・マウス・スライドボタンの表示
   ScrollTrigger.create({
-    trigger: '.js_scroll_TopImage_trigger',
+    trigger: '.JS_scroll_trigger',
     start: 'center+=10 center',
     end: 'center+=10 bottom',
     markers: false,
     onLeaveBack: () => {
-      $('.js_scroll_NavHeader_visible_target').removeClass('isVisible');
-      $('.js_scroll_mouse_invisible_target').removeClass('isInvisible');
-      $('.js_scroll_menuBtn_close_target').removeClass('isActive-button');
-      $('.js_scroll_menu_close_target').removeClass('isOpen-menu');
+      $('.JS_scroll_isShow_target').addClass('isHide');
+      $('.JS_scroll_isHide_target').removeClass('isHide');
+      $('.JS_scroll_isPassive_target').removeClass('C_button--hamberger--isActive');
+      $('.JS_scroll_isClose_target').removeClass('isOpen');
     },
     onLeave: () => {
-      $('.js_scroll_NavHeader_visible_target').addClass('isVisible');
-      $('.js_scroll_mouse_invisible_target').addClass('isInvisible');
+      $('.JS_scroll_isShow_target').removeClass('isHide');
+      $('.JS_scroll_isHide_target').addClass('isHide');
     }
   });
 });

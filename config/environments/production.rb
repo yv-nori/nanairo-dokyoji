f979fcdd3fe2c6bc1dbdf346a47a03295bae866a
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-02-03 15:58:41 +0900

    assets.digest = trueにした

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
   config.assets.compile = true
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :scss
+  config.assets.digest = true 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 

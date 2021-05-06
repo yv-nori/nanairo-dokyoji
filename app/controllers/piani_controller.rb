b93a8559f8af4f038836484c68fcada8579e1b6f
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-05-06 12:57:55 +0900

    写真を適正サイズに変更

--- app/controllers/piani_controller.rb
+++ app/controllers/piani_controller.rb
@@ -272,6 +272,94 @@ class PianiController < ApplicationController
       },
     ]
   end
+  def one_day
+    @items = [
+      {
+        time: '7:00',
+        title: '順次登園'
+        class: '',
+        text: '一人ひとりの<br>
+                その日の体調を診た後<br>
+                好きな遊びをします'
+      },
+      {
+        time: '9:30',
+        title: '朝の集い'
+        class: 'toddler',
+        text: '朝の歌を歌い<br>
+              一日の流れを聞きます'
+      },
+      {
+        time: '9:30',
+        title: '保育士と遊ぶ'
+        class: 'infant',
+        text: 'お天気のよい日は戸外へ<br>
+                室内ではわらべうたや<br>
+                絵本などを楽しみます'
+      },
+      {
+        time: '10:00',
+        title: '体操'
+        class: 'toddler',
+        text: '園庭に出て体操をします'
+      },
+      {
+        time: '10:30',
+        title: 'クラス活動'
+        class: 'toddler',
+        text: '製作やリズム遊びをして<br>
+                楽しく学びます'
+      },
+      {
+        time: '11:15',
+        title: '給食'
+        class: 'infant',
+        text: '保育教諭の援助のもと<br>
+                楽しく食事をします'
+      },
+      {
+        time: '11:45',
+        title: '給食'
+        class: 'toddler',
+        text: '好き嫌いを無くし<br>
+                食事のマナーを<br>
+                身に付けます'
+      },
+      {
+        time: '13:30',
+        title: 'クラスでの活動'
+        class: 'toddler',
+        text: '各クラス楽しく学びます'
+      },
+      {
+        time: '12:30',
+        title: '午睡'
+        class: 'infant',
+        text: '静かな環境の中で<br>
+                お昼寝をします'
+      },
+      {
+        time: '14:30',
+        title: '午睡'
+        class: '',
+        text: 'みんなで仲良く<br>
+                おやつを食べます'
+      },
+      {
+        time: '16:30',
+        title: '順次降園'
+        class: '',
+        text: '荷物の整理整頓をします'
+      },
+      {
+        time: '19:00',
+        title: '延長保育'
+        class: '',
+        text: '合同保育をしながら<br>
+                お迎えを待ちます'
+      }
+    ]
+  end
   def index
     @links = {
       about: about_piani_index_path,

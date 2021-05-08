ba3ea8bfe992ca42ca56955ee806863871875325
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-05-09 06:18:27 +0900

    ダミー画像の挿入

--- app/controllers/piani_controller.rb
+++ app/controllers/piani_controller.rb
@@ -276,7 +276,7 @@ class PianiController < ApplicationController
     @items = [
       {
         time: '7:00',
-        title: '順次登園'
+        title: '順次登園',
         class: '',
         text: '一人ひとりの<br>
                 その日の体調を診た後<br>
@@ -284,14 +284,14 @@ class PianiController < ApplicationController
       },
       {
         time: '9:30',
-        title: '朝の集い'
+        title: '朝の集い',
         class: 'toddler',
         text: '朝の歌を歌い<br>
               一日の流れを聞きます'
       },
       {
         time: '9:30',
-        title: '保育士と遊ぶ'
+        title: '保育士と遊ぶ',
         class: 'infant',
         text: 'お天気のよい日は戸外へ<br>
                 室内ではわらべうたや<br>
@@ -299,27 +299,27 @@ class PianiController < ApplicationController
       },
       {
         time: '10:00',
-        title: '体操'
+        title: '体操',
         class: 'toddler',
         text: '園庭に出て体操をします'
       },
       {
         time: '10:30',
-        title: 'クラス活動'
+        title: 'クラス活動',
         class: 'toddler',
         text: '製作やリズム遊びをして<br>
                 楽しく学びます'
       },
       {
         time: '11:15',
-        title: '給食'
+        title: '給食',
         class: 'infant',
         text: '保育教諭の援助のもと<br>
                 楽しく食事をします'
       },
       {
         time: '11:45',
-        title: '給食'
+        title: '給食',
         class: 'toddler',
         text: '好き嫌いを無くし<br>
                 食事のマナーを<br>
@@ -327,33 +327,33 @@ class PianiController < ApplicationController
       },
       {
         time: '13:30',
-        title: 'クラスでの活動'
+        title: 'クラスでの活動',
         class: 'toddler',
         text: '各クラス楽しく学びます'
       },
       {
         time: '12:30',
-        title: '午睡'
+        title: '午睡',
         class: 'infant',
         text: '静かな環境の中で<br>
                 お昼寝をします'
       },
       {
         time: '14:30',
-        title: '午睡'
+        title: '午睡',
         class: '',
         text: 'みんなで仲良く<br>
                 おやつを食べます'
       },
       {
         time: '16:30',
-        title: '順次降園'
+        title: '順次降園',
         class: '',
         text: '荷物の整理整頓をします'
       },
       {
         time: '19:00',
-        title: '延長保育'
+        title: '延長保育',
         class: '',
         text: '合同保育をしながら<br>
                 お迎えを待ちます'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/ujs").start();
 require("@rails/activestorage").start();
 require("channels");
 require("jquery");
-require("../function/lazysizes.min.js");
+require("../function/lazysizes.min");
 // ページ関数
 let CurrentPath = location.pathname;
 if (CurrentPath == "/nanairo/recruit" ||

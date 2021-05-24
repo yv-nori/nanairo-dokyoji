e46f52f81daaa55727f69864a25eaa9efd922aff
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-05-24 12:36:34 +0900

    seed_fu導入

---
+++ db/fixtures/category.rb
@@ -0,0 +1,51 @@
+Category.seed do |s|
+  s.name = 'パルティお知らせ(全体)'
+end
+Category.seed do |s|
+  s.name = 'ピアニィお知らせ(全体)'
+end
+Category.seed do |s|
+  s.name = 'パルティおたより(パスワード)'
+end
+Category.seed do |s|
+  s.name = 'ピアニィおたより(パスワード)'
+end
+Category.seed do |s|
+  s.name = 'パルティムービー(パスワード)'
+end
+Category.seed do |s|
+  s.name = 'ピアニィムービー(パスワード)'
+end
+Category.seed do |s|
+  s.name = 'パルティ書式'
+end
+Category.seed do |s|
+  s.name = 'ピアニィ書式'
+end
+Category.seed do |s|
+  s.name = '求人票'
+end
+Category.seed do |s|
+  s.name = '定款'
+end
+Category.seed do |s|
+  s.name = '役員名簿'
+end
+Category.seed do |s|
+  s.name = '役員報酬基準'
+end
+Category.seed do |s|
+  s.name = 'パルティ自己評価表'
+end
+Category.seed do |s|
+  s.name = 'ピアニィ自己評価表'
+end
+Category.seed do |s|
+  s.name = 'パルティ募集要項'
+end
+Category.seed do |s|
+  s.name = 'ピアニィ募集要項'
+end
+Category.seed do |s|
+  s.name = 'tesut'
+end
---
+++ db/fixtures/user.rb
@@ -0,0 +1,12 @@
+User.seed do |s|
+  s.name = 'nanairo_root'
+  s.password = 'hozdkz8969'
+end
+User.seed do |s|
+  s.name = 'parti_user', 
+  s.password = 'houpublic'
+end
+User.seed do |s|
+  s.name = 'piani_user', 
+  s.password = 'houpublic'
+end

75f2cc457103fa92e35b2cefe05931b959e088fe
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-05-24 11:55:44 +0900

    修正

--- config/initializers/const_model_user.rb
+++ config/initializers/const_model_user.rb
@@ -1,5 +1,20 @@
 module USER
-  NanairoRoot_ID = User.where(name: 'nanairo_root').first.id
-  PartiUser_ID = User.where(name: 'parti_user').first.id
-  PianiUser_ID = User.where(name: 'piani_user').first.id
+  @var = User.where(name: 'nanairo_root').first.id
+  if @var ==! nil
+    NanairoRoot_ID = @var
+  else
+    NanairoRoot_ID = 1
+  end
+  @var = User.where(name: 'parti_user').first.id
+  if @var ==! nil
+    PartiUser_ID = @var
+  else
+    PartiUser_ID = 2
+  end
+  @var = User.where(name: 'piani_user').first.id
+  if @var ==! nil
+    PianiUser_ID = @var
+  else
+    PianiUser_ID = 3
+  end
 end

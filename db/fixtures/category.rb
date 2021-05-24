3fbf32bbde203fb118aec3d8bbb23d72edf29e19
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-05-24 12:58:28 +0900

    seed_fu修正

--- db/fixtures/category.rb
+++ db/fixtures/category.rb
@@ -1,51 +1,68 @@
 Category.seed do |s|
+  s.id = 1
   s.name = 'パルティお知らせ(全体)'
 end
 Category.seed do |s|
+  s.id = 2
   s.name = 'ピアニィお知らせ(全体)'
 end
 Category.seed do |s|
+  s.id = 3
   s.name = 'パルティおたより(パスワード)'
 end
 Category.seed do |s|
+  s.id = 4
   s.name = 'ピアニィおたより(パスワード)'
 end
 Category.seed do |s|
+  s.id = 5
   s.name = 'パルティムービー(パスワード)'
 end
 Category.seed do |s|
+  s.id = 6
   s.name = 'ピアニィムービー(パスワード)'
 end
 Category.seed do |s|
+  s.id = 7
   s.name = 'パルティ書式'
 end
 Category.seed do |s|
+  s.id = 8
   s.name = 'ピアニィ書式'
 end
 Category.seed do |s|
+  s.id = 9
   s.name = '求人票'
 end
 Category.seed do |s|
+  s.id = 10
   s.name = '定款'
 end
 Category.seed do |s|
+  s.id = 11
   s.name = '役員名簿'
 end
 Category.seed do |s|
+  s.id = 12
   s.name = '役員報酬基準'
 end
 Category.seed do |s|
+  s.id = 13
   s.name = 'パルティ自己評価表'
 end
 Category.seed do |s|
+  s.id = 14
   s.name = 'ピアニィ自己評価表'
 end
 Category.seed do |s|
+  s.id = 15
   s.name = 'パルティ募集要項'
 end
 Category.seed do |s|
+  s.id = 16
   s.name = 'ピアニィ募集要項'
 end
 Category.seed do |s|
+  s.id = 17
   s.name = 'tesut'
 end
--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -1,16 +1,15 @@
 User.seed do |s|
+  s.id = 1
   s.name = 'nanairo_root'
   s.password = 'hozdkz8969'
 end
 User.seed do |s|
+  s.id = 2
   s.name = 'parti_user', 
   s.password = 'houpublic'
 end
 User.seed do |s|
+  s.id = 3
   s.name = 'piani_user', 
-  s.password = 'houpublic'
-end
-User.seed do |s|
-  s.name = 'text', 
   s.password = 'houpublic'
 end

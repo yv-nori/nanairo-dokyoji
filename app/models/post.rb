abfe0748acde29df8d5a39d1a9549f10b225ff80
yv-nori <74577570+yv-nori@users.noreply.github.com>
2021-06-30 18:29:49 +0900

    役員名簿表示されないので修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :category
   has_one_attached :image
-  @var = Post.where(category_id: Category_id::JobPosting_ID).order(updated_at: 'ASC').first
+  @var = Post.where(category_id: Category_id::JobPosting_ID).order(updated_at: :desc).first
   if @var === nil
     JobPosting = nil
     JobPosting_Update = Date.new(2020, 4, 1)
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
     JobPosting = @var
     JobPosting_Update = @var.updated_at
   end
-  @var = Post.with_attached_image.where(category_id: Category_id::CompanyStatute_ID).order(updated_at: 'ASC').first
+  @var = Post.with_attached_image.where(category_id: Category_id::CompanyStatute_ID).order(updated_at: :desc).first
   if @var === nil
     CompanyStatute = nil
     CompanyStatute_Update = Date.new(2020, 4, 1)
@@ -17,7 +17,7 @@ class Post < ApplicationRecord
     CompanyStatute = @var
     CompanyStatute_Update = @var.updated_at
   end
-  @var = Post.with_attached_image.where(category_id: Category_id::OfficerList_ID).order(updated_at: 'ASC').first
+  @var = Post.with_attached_image.where(category_id: Category_id::OfficerList_ID).order(updated_at: :desc).first
   if @var === nil
     OfficerList = nil
     OfficerList_Update = Date.new(2020, 4, 1)
@@ -25,7 +25,7 @@ class Post < ApplicationRecord
     OfficerList = @var
     OfficerList_Update = @var.updated_at
   end
-  @var = Post.with_attached_image.where(category_id: Category_id::PaymentRule_ID).order(updated_at: 'ASC').first
+  @var = Post.with_attached_image.where(category_id: Category_id::PaymentRule_ID).order(updated_at: :desc).first
   if @var === nil
     PaymentRule = nil
     PaymentRule_Update = Date.new(2020, 4, 1)
@@ -33,7 +33,7 @@ class Post < ApplicationRecord
     PaymentRule = @var
     PaymentRule_Update = @var.updated_at
   end
-  @var = Post.with_attached_image.where(category_id: Category_id::PartiSelfAssessment_ID).order(updated_at: 'ASC').first
+  @var = Post.with_attached_image.where(category_id: Category_id::PartiSelfAssessment_ID).order(updated_at: :desc).first
   if @var === nil
     PartiSelfAssessment = nil
     PartiSelfAssessment_Update = Date.new(2020, 4, 1)
@@ -41,7 +41,7 @@ class Post < ApplicationRecord
     PartiSelfAssessment = @var
     PartiSelfAssessment_Update = @var.updated_at
   end
-  @var = Post.with_attached_image.where(category_id: Category_id::PianiSelfAssessment_ID).order(updated_at: 'ASC').first
+  @var = Post.with_attached_image.where(category_id: Category_id::PianiSelfAssessment_ID).order(updated_at: :desc).first
   if @var === nil
     PianiSelfAssessment = nil
     PianiSelfAssessment_Update = Date.new(2020, 4, 1)
@@ -49,7 +49,7 @@ class Post < ApplicationRecord
     PianiSelfAssessment = @var
     PianiSelfAssessment_Update = @var.updated_at
   end
-  @var = Post.with_attached_image.where(category_id: Category_id::PartiAdmission_ID).order(updated_at: 'ASC').first
+  @var = Post.with_attached_image.where(category_id: Category_id::PartiAdmission_ID).order(updated_at: :desc).first
   if @var === nil
     PartiAdmission_PDF = nil
     PartiAdmission_PDF_Update = Date.new(2020, 4, 1)
@@ -57,7 +57,7 @@ class Post < ApplicationRecord
     PartiAdmission_PDF = @var
     PartiAdmission_PDF_Update = @var.updated_at
   end
-  @var = Post.with_attached_image.where(category_id: Category_id::PianiAdmission_ID).order(updated_at: 'ASC').first
+  @var = Post.with_attached_image.where(category_id: Category_id::PianiAdmission_ID).order(updated_at: :desc).first
   if @var === nil
     PianiAdmission_PDF = nil
     PianiAdmission_PDF_Update = Date.new(2020, 4, 1)
